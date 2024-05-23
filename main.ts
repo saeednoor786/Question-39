50d18c5390b7f2d6118f29c9ae4c25e6965317ed
Saeed Noor <163897168+saeednoor786@users.noreply.github.com>
2024-05-24 00:15:13 +0500

    thirtynine assignment

---
+++ main.ts
@@ -0,0 +1,21 @@
+//Q:39 City Names: Write a function called city_country() that takes in the name of a city and 
+// its country. The function should return a string formatted like this:
+
+// "Lahore, Pakistan"
+
+// Call your function with at least three city-country pairs, and print the value that’s returned.
+
+
+function country_city (city: string, country: string): string{
+    return `${city}, ${country}`;
+}
+
+//  calling function with three citi_country pairs
+let city1: string = country_city("lahore", "pakistan");
+let city2: string = country_city("new york", "USA");
+let city3: string = country_city("paris", "France");
+
+// printing the value return by the function 
+console.log(city1);
+console.log(city2);
+console.log(city3);
